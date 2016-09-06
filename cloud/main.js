dda2a4bf97b8009c48ac7f0809cb15df8574a48d
Avery <avery@Averys-iMac.local>
2016-09-06 22:04:19 +0800

    add js include

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,19 @@
+require("cloud/promotion_function.js");
+require("cloud/login_function.js");
+require("cloud/google_service.js"); 
+require("cloud/db_trigger.js");
+require("cloud/job_function.js");
+require("cloud/store_management_function.js");
+require("cloud/web.js");
+require("cloud/storeapp-demo.js");
+require("cloud/customer_service_management_function.js");
+require("cloud/call-bee.js");
+
+var util = require("cloud/util.js");
+var _ = require("underscore");
+var logger = require("cloud/mail_service.js");
+var Image = require("parse-image");
+var prop = require("cloud/app_properties.js");
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
