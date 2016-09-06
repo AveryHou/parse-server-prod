a3239563a6337784398fc8e8ba2c76970d033c8e
Avery <avery@Averys-iMac.local>
2016-09-06 22:14:26 +0800

    modify main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,3 @@
-require("cloud/promotion_function.js");
-require("cloud/login_function.js");
-require("cloud/google_service.js"); 
-require("cloud/db_trigger.js");
-require("cloud/job_function.js");
-require("cloud/store_management_function.js");
-require("cloud/web.js");
-require("cloud/storeapp-demo.js");
-require("cloud/customer_service_management_function.js");
-require("cloud/call-bee.js");
-
 
 
 Parse.Cloud.define('hello', function(req, res) {
