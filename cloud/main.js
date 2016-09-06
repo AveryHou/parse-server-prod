d2cf012f9f29e2bf02547797c0ac4e3cde0cdfc8
Avery <avery@Averys-iMac.local>
2016-09-06 22:56:12 +0800

    update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,7 +2,7 @@ require("./promotion_function.js");
 //require("./login_function.js");
 
 var prop = require("./app_properties.js");
-//var Mailgun = require('mailgun');
+var Mailgun = require('mailgun');
 //Mailgun.initialize(prop.mailgun_domain(), prop.mailgun_key());
 
 Parse.Cloud.define('hello', function(req, res) {
